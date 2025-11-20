d5c4b775920b15f99e1991be4757c717735b7fd1
Charles Anthony <charlesanthony296@gmail.com>
2025-11-20 13:31:20 +0100

    action changes

---
+++ generator-service/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+import time
+import math
+import random
+import requests
+
+TARGET_URL = os.getenv("TARGET_URL", "http://localhost:8081/ingest")
+SESSION_ID = os.getenv("SESSION_ID", "s1")
+RATE_HZ = float(os.getenv("RATE_HZ", "10"))
+DT = 1.0 / RATE_HZ
+
+
+def sample(t):
+    f = 1.8
+    
