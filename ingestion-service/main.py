6663525a3c8d475704bf6ef74bfcb468ac1516b3
Charles Anthony <charlesanthony296@gmail.com>
2025-12-22 23:35:56 +0100

    health and ingest routes added

--- ingestion-service/main.py
+++ ingestion-service/main.py
@@ -8,3 +8,50 @@ app = FastAPI(title ="Ingestion service")
 buffer_size = 128
 buffer = []
 
+
+anomaly_url = "http://anomaly:8080/predict"
+
+class SensorSample(BaseModel):
+    AccV: float
+    AccML: float
+    AccAP: float
+
+
+@app.get("/health")
+def health():
+    return {
+        "ok": True,
+        "buffer_size": len(buffer),
+        "required": buffer_size
+    }
+
+@app.post("/ingest")
+def ingest(sample: SensorSample):
+
+    buffer.append([sample.AccV, sample.AccML, sample.AccAP])
+
+    if len(buffer) < buffer_size:
+        return {
+            "ready": False,
+            "buffered": len(buffer)
+        }
+    
+    payload = {
+        "features": buffer[:buffer_size]
+    }
+
+    buffer.clear()
+
+    # send it to the anomaly service
+    try:
+        response = requests.post(anomaly_url, json=payload, timeout=3)
+        return {
+            "ready": True,
+            "anomaly_response": response.json()
+        }
+    
+    except Exception as e:
+        return {
+            "ready": False,
+            "error": str(e)
+        }
