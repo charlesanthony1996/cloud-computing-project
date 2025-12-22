6898a01fcbf7282593d9011fde8b079cedb22296
Charles Anthony <charlesanthony296@gmail.com>
2025-12-22 22:44:53 +0100

    main.py starting changes for ingestion

--- ingestion-service/main.py
+++ ingestion-service/main.py
@@ -1,3 +1,10 @@
-import time
-import numpy as np
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+
+app = FastAPI(title ="Ingestion service")
+
+# internal buffer
+buffer_size = 128
+buffer = []
 
